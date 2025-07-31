91f37ebb7ecae449e446cee12e249d9c916ae975
Lukesh Tiwari <lukeshtiwari.devil2@gmail.com>
2025-07-31 15:43:34 +0530

    Fourth commit where i did the authentication part with the help of puter js

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,12 @@
 import type { Route } from "./+types/home";
-import Navbar from "/components/Navbar";
-import { resumes } from "constants";
-import ResumeCard from "/components/ResumeCard";
+import { useEffect } from "react";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import ResumeCard from "components/ResumeCard";
+import Navbar from "components/Navbar";
+import { resumes } from "constants/index";
+
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "LukeATS" },
@@ -10,24 +15,32 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const {isLoading,auth}= usePuterStore();
+  const navigate =useNavigate();
+
+ useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
+ 
   return <main className="bg-[url('/images/bg-main.svg')]">
     <Navbar></Navbar>
 
 <section className="main-section">
-<div className="page-heading">
+<div className="page-heading py-16">
   <h1>Track Your Applications & Resume Ratings</h1>
   <h2>Review  your submissions and check AI-powered feedback.</h2>
 </div>
-</section>
 
   {resumes.length > 0 && ( 
     <div className="resumes-section">
-      {resumes.map((resume) => (
+      {resumes.map((resume: Resume) => (
         <ResumeCard key={resume.id} resume={resume} />
       ))}
 
     </div>
   )}
+  </section>
 
   </main>
 }
--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,6 @@
 
 
 
-
 export const resumes: Resume[] = [
   {
     id: "1",
